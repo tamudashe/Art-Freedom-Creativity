4ed911377b59a5f81266546a295285ae5bda99c2
tamudashe <tamudashe@gmail.com>
2019-07-26 12:14:38 -0400

    clean code

--- redesign/script.js
+++ redesign/script.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    window.addEventListener("load", function (e) {
+document.addEventListener("DOMContentLoaded", function(event) {
+    window.addEventListener("load", function(e) {
         document.body.style.display = "block";
 
         const tl = new TimelineMax();
-        tl.staggerFrom("section", 2,
-            {
-                opacity: 0,
-                scale: .5,
-                ease: Power2.easeOut
-            }, 0.2)
+        tl.staggerFrom("section", 2, {
+            opacity: 0,
+            scale: .5,
+            ease: Power2.easeOut
+        }, 0.2)
         tl.staggerFrom("h1, h2", .5, {
             opacity: 0,
             y: -40,
-            ease: Power2.easeInOut}, 0.2, "-=2")
+            ease: Power2.easeInOut
+        }, 0.2, "-=2")
         tl.from(".sub-wrapper", 1, {
             opacity: 0,
-                y: -40,
-                ease: Power2.easeInOut
-            }, 0.2, "-=1.5")
+            y: -40,
+            ease: Power2.easeInOut
+        }, 0.2, "-=1.5")
     }, false);
 });
-
-
